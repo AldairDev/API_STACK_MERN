9970b209b4b39bfd83204c1fc8bad47bf1739af5
Aldair <aldairsosa18@gmail.com>
2019-11-29 00:13:34 -0500

    added product model

--- server/models/product.js
+++ server/models/product.js
@@ -1,1 +1,24 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+const product = new Schema({
+    
+    product_owner: String,
+    name: {
+        type: String,
+        required : true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String
+    },
+    description: String,
+}, {
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model('products', product);
